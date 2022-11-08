b265765f5e57da19bfb72a3435497853d2201cb9
Yixiao Chen <yixiaochenyc@gmail.com>
2022-11-08 15:41:44 +0800

    new dir structure

--- prepare-host.js
+++ prepare-host.js
@@ -3,7 +3,8 @@ const { exec } = require("child_process")
 
 const copyFiles = async () => {
   try {
-    
+    await fs.copy('./WV3D-MVP/package/dist', './node_modules/@pdftron/webviewer-bim-client/dist');
+
     await fs.copy('./node_modules/@pdftron/webviewer/public', './public/webviewer/lib');
     await fs.copy('./node_modules/@pdftron/webviewer-bim-client/dist', './public/webviewer-bim/');
     console.log('WebViewer and WebViewer BIM files copied over successfully.');
@@ -11,7 +12,7 @@ const copyFiles = async () => {
     console.error(err);
   }
 };
-exec('npm cache clear --force && cd ../WV3D-MVP && rm -rf dist && npm run build && npm pack && mv pdftron-webviewer-bim-client*.tgz ../startbim/1.tgz && cd ../startbim && rm -rf package && tar -xzvf 1.tgz && rm -rf ./node_modules/@pdftron/webviewer-bim-client && npm i @pdftron/webviewer-bim-client', (err, stdout, stderr) => {
+exec('cd WV3D-MVP && npm run build && cd ..', (err, stdout, stderr) => {
   if (err) {
     console.error(err);
     return;
